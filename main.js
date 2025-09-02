779e5cd457145983d40b559cf456eaf85337cd88
Ylenia <05ylenia@gmail.com>
2025-09-02 10:05:14 +0200

    Fotos-Versuch

--- main.js
+++ main.js
@@ -10,32 +10,31 @@ window.addEventListener('load', function() {
     }
 });
 
-//Slideshow
 let slideIndex = 1;
 showSlides(slideIndex);
 
-// Vorhäriges/nächstes Foto
+// Weiter/Zurück
 function plusSlides(n) {
-    showSlides(slideIndex += n);
+  showSlides(slideIndex += n);
 }
 
-// Thumbnail image controls
+// Direkt ansteuern
 function currentSlide(n) {
-    showSlides(slideIndex = n);
-};
+  showSlides(slideIndex = n);
+}
 
 function showSlides(n) {
-    let i;
-    let slides = document.getElementsByClassName("mySlides");
-    let dots = document.getElementsByClassName("dot");
-    if (n > slides.length) { slideIndex = 1 }
-    if (n < 1) { slideIndex = slides.length }
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
-};
+  let i;
+  let slides = document.getElementsByClassName("slide");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
